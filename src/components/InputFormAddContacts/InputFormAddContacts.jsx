6db7fdd71315dbb211f38fa1faba2a74508328d8
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-09-22 09:06:17 +0100

    Update InputFormAddContacts.jsx

--- src/components/InputFormAddContacts/InputFormAddContacts.jsx
+++ src/components/InputFormAddContacts/InputFormAddContacts.jsx
@@ -58,9 +58,4 @@ export const InputFormPhone = () => {
                     type="text"
                     name="name"
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required/>
-            </lable>
-            <lable htmlFor="phoneNumber">
-           
-  
+            
