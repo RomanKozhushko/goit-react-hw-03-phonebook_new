be39c3c590601c776e5327d7f8691c0e7e85d873
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-09-22 09:05:57 +0100

    Update InputFormAddContacts.jsx

--- src/components/InputFormAddContacts/InputFormAddContacts.jsx
+++ src/components/InputFormAddContacts/InputFormAddContacts.jsx
@@ -62,12 +62,5 @@ export const InputFormPhone = () => {
                     required/>
             </lable>
             <lable htmlFor="phoneNumber">
-                Phone number
-                <Input
-                    type="tel"
-                    name="phoneNumber"
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                    />  
+           
   
