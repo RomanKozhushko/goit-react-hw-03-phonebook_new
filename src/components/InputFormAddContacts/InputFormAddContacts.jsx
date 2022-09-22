e24f8bf6aab88a71993120a9da0b8c84d8ca0ef6
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-09-22 09:05:38 +0100

    Update InputFormAddContacts.jsx

--- src/components/InputFormAddContacts/InputFormAddContacts.jsx
+++ src/components/InputFormAddContacts/InputFormAddContacts.jsx
@@ -47,3 +47,32 @@ export const InputFormPhone = () => {
         </Formik>
     );
 }
+
+
+
+        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form autoComplete="off">
+            <lable htmlFor="name">
+                Full Name
+                <Input
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required/>
+            </lable>
+            <lable htmlFor="phoneNumber">
+                Phone number
+                <Input
+                    type="tel"
+                    name="phoneNumber"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                    />  
+            </lable>
+                <button type="submit">Add</button>
+            </Form>
+        </Formik>
+    );
+}
