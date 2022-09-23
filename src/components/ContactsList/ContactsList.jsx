f85111dedf756ce9fc129f76f3756b8028385bfe
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-09-23 13:02:57 +0100

    Filter

---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,18 @@
+import { ContactItem } from "./ContactItem";
+import PropTypes from 'prop-types';
+
+export function ContactList({ contacts, onDelete }) {
+
+    return (
+        <>
+            {contacts.map((contact, idx) => (
+                <ContactItem key={contact.id} contact={contact} idx={idx} method={onDelete}/>
+            ))}
+        </>
+    )
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object),
+  onDelete: PropTypes.func,
+}
